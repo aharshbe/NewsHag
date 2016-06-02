c8e7b092e047a13fa4f99fe0572e5e6d8d5cd818
aaron <aarongitis@gmail.com>
2016-06-01 17:24:46 -0700

    Add Model

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
@@ -1,7 +1,12 @@
 package badassapps.aaron.newshag;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Will inflate our menu's search functionality
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+        inflater.inflate(R.menu.access_db, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
