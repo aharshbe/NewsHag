9c4b9c03fcb3ca8f253492dfccc2a1d9f5c75e52
aharshbe <a.catawampus@gmail.com>
2016-06-01 20:26:23 -0700

    Adding the progress bar stuff for the webview to make it look a little nicer

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/WebViewForFavorites.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/WebViewForFavorites.java
@@ -1,23 +1,53 @@
 package badassapps.aaron.newshag;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class WebViewForFavorites extends AppCompatActivity {
 
-    WebView webView ;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view_for_favorites);
 
-
         webView = (WebView) findViewById(R.id.loadStory);
 
-        webView.setWebViewClient(new WebViewClient());
+        final ProgressDialog pd = ProgressDialog.show(this, "", "Loading...", true);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pd.setMessage("Sorry, still loading");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (pd != null && pd.isShowing()) {
+                    pd.dismiss();
+                }
+            }
+        });
+
+
         webView.loadUrl("http://www.nytimes.com/2016/06/02/world/europe/mikhail-gorbachev-interview-vladimir-putin.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=second-column-region&region=top-news&WT.nav=top-news&_r=0");
+
     }
+
+
 }
+
+
