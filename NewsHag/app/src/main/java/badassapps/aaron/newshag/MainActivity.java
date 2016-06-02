59bfe5f873f59ba2120bdb72ccdcf9902f998f50
aaron <aarongitis@gmail.com>
2016-06-02 12:42:59 -0700

    Add more stuff

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
@@ -1,24 +1,32 @@
 package badassapps.aaron.newshag;
 
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.SearchManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 
+import android.support.v4.media.MediaBrowserCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.SearchView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 
 import android.view.Menu;
@@ -29,6 +37,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,12 +46,26 @@ import java.util.ArrayList;
 
 
 
-public class MainActivity extends AppCompatActivity implements Top10Model.ApiResponseHandler{
+public class MainActivity extends AppCompatActivity{
 
     ArrayList<Article> mList;
     CustomAdapter adapter;
     ListView listView;
     LayoutInflater layoutInflater;
+
+    // Constants
+    // Content provider authority
+    public static final String AUTHORITY = "badassapps.aaron.newshag.AppContentProvider";
+    // Account type
+    public static final String ACCOUNT_TYPE = "example.com";
+    // Account
+    public static final String ACCOUNT = "default_account";
+
+    Account mAccount;
+
+    // Global variables
+    // A content resolver for accessing the provider
+    ContentResolver mResolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,32 +74,11 @@ public class MainActivity extends AppCompatActivity implements Top10Model.ApiRes
 
         checkFirstRun();
 
-
+        mAccount = createSyncAccount(this);
         mList = new ArrayList<>();
         listView = (ListView) findViewById(R.id.listView);
         adapter = new CustomAdapter(this, R.layout.list_items, mList);
         listView.setAdapter(adapter);
-
-
-        Article article = new Article("www.google.com", "This is the image", "This is the title");
-        Article article2 = new Article("www.yahoo.com", "This is the image", "This is the title");
-        Article article3 = new Article("www.yahoo.com", "This is the image", "This is the title");
-        Article article4 = new Article("www.yahoo.com", "This is the image", "This is the title");
-        Article article5 = new Article("www.yahoo.com", "This is the image", "This is the title");
-        Article article6 = new Article("www.yahoo.com", "This is the image", "This is the title");
-
-
-        mList.add(article);
-
-        mList.add(article2);
-
-        mList.add(article3);
-
-        mList.add(article4);
-
-        mList.add(article5);
-
-        mList.add(article6);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -90,6 +92,33 @@ public class MainActivity extends AppCompatActivity implements Top10Model.ApiRes
         });
 
 
+        //Step 1 (for content resolver)
+        //new Handler
+        getContentResolver().registerContentObserver(AppContentProvider.CONTENT_URI,true,new
+                NewsContentObserver
+                (new Handler()));
+
+        //Performs manual sync
+        Bundle settingsBundle = new Bundle();
+        settingsBundle.putBoolean(
+                ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        settingsBundle.putBoolean(
+                ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        /*
+         * Request the sync for the default account, authority, and
+         * manual sync settings
+         */
+
+        //REQUESTS A SYNC FOR THE ACCOUNT
+        //i.e. if there's no cache, or app hasn't been used for several days...
+        ContentResolver.requestSync(mAccount, AUTHORITY, settingsBundle);
+
+        ContentResolver.setSyncAutomatically(mAccount,AUTHORITY,true);
+        ContentResolver.addPeriodicSync(
+                mAccount,
+                AUTHORITY,
+                Bundle.EMPTY,
+                60);
     }
 
     private void NOTIFICATIONisAllowed() {
@@ -162,7 +191,7 @@ public class MainActivity extends AppCompatActivity implements Top10Model.ApiRes
         adapter = new CustomAdapter(this, R.layout.list_items, mList);
         listView.setAdapter(adapter);
 
-
+        //Gut this out later!!
         Article article = new Article("www.google.com", "This is the image", "This is the title");
         Article article2 = new Article("www.yahoo.com", "This is the image", "This is the title");
         Article article3 = new Article("www.yahoo.com", "This is the image", "This is the title");
@@ -236,8 +265,6 @@ public class MainActivity extends AppCompatActivity implements Top10Model.ApiRes
 
 
     //Will inflate our menu's search functionality
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -297,12 +324,57 @@ public class MainActivity extends AppCompatActivity implements Top10Model.ApiRes
         }
     }
 
-    @Override
-    public void handleResponse(ArrayList<Article> response) {
-        mList = response;
-        adapter.clear();
-        adapter.notifyDataSetChanged();
-        adapter.addAll(mList);
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account createSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
+    }
+
+    public class NewsContentObserver extends ContentObserver {
+
+        /**
+         * Creates a content observer.
+         *
+         * @param handler The handler to run {@link #onChange} on, or null if none.
+         */
+        public NewsContentObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            //do stuff on UI thread
+//            adapter.swapCursor
+//                    (getContentResolver().query(AppContentProvider.CONTENT_URI, null, null,
+//                    null, null));
+        }
     }
 }
 
