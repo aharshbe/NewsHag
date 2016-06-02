e787290f2b414b79333e8ffb75c61fa00ba3a673
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-06-01 17:28:36 -0700

    Adds StoryDetailActivity

---
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/StoryDetailActivity.java
@@ -0,0 +1,13 @@
+package badassapps.aaron.newshag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StoryDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story_detail);
+    }
+}
