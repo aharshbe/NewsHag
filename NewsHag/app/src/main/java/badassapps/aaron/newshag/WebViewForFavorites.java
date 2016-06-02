9f12333a3393dd940d31032ecbdfa90d48fda078
aharshbe <a.catawampus@gmail.com>
2016-06-01 18:31:29 -0700

    Adding the custom adapter to favorites and the delete

---
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/WebViewForFavorites.java
@@ -0,0 +1,13 @@
+package badassapps.aaron.newshag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebViewForFavorites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_for_favorites);
+    }
+}
