858b2d396103ee4a5f622e602a349945826c02e5
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-06-02 11:18:10 -0700

    Adds content provider class and more to db

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
