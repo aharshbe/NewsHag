02d1c5239dd5f6cd6c5d84dfd40cf6f026dadf63
aharshbe <a.catawampus@gmail.com>
2016-06-03 16:46:39 -0700

    Adding an animation to the splash though boring

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/SplashScreen.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/SplashScreen.java
@@ -3,6 +3,9 @@ package badassapps.aaron.newshag;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,12 +14,23 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        ImageView imageView = (ImageView) findViewById(R.id.splash);
+
+
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left);
+        animation.setDuration(1000);
+
+        imageView.setAnimation(animation);
+
+
+
         Thread background = new Thread() {
             public void run() {
 
                 try {
                     // Thread will sleep for 5 seconds
-                    sleep(1 * 1000);
+                    sleep(3 * 1000);
 
                     // After 5 seconds redirect to another intent
                     Intent i = new Intent(getBaseContext(), NavD.class);
