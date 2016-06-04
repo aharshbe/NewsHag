179473868fe58723795153e78b3c288e57852b29
aharshbe <a.catawampus@gmail.com>
2016-06-03 18:00:10 -0700

    latest version of code for tianna

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/SplashScreen.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/SplashScreen.java
@@ -58,7 +58,7 @@ public class SplashScreen extends AppCompatActivity {
         news.setAnimation(animation5);
 
         Animation animation6 = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_out_right);
-        animation6.setDuration(3000);
+        animation6.setDuration(2700);
 
         urdaily.setAnimation(animation6);
 
@@ -72,7 +72,7 @@ public class SplashScreen extends AppCompatActivity {
 
                 try {
                     // Thread will sleep for 5 seconds
-                    sleep(4 * 1000);
+                    sleep(3 * 1000);
 
                     // After 5 seconds redirect to another intent
                     Intent i = new Intent(getBaseContext(), NavD.class);
